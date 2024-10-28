219690f9ca2bdd2f90f33321bb82e5d79fdf5137
Francesco Amadori <francesco.amadori@inaf.it>
2024-10-28 19:12:39 +0100

    Allow multiple limb profile selection

--- src/daneel/detection/transit_yaml.py
+++ src/daneel/detection/transit_yaml.py
@@ -1,5 +1,5 @@
 import batman
-from numpy import linspace, array
+from numpy import linspace, array, isnan
 import matplotlib
 from pathlib import Path
 from pandas import read_csv
@@ -9,18 +9,23 @@ matplotlib.use('Qt5Agg')
 
 # This function return the coefficients for the
 # quadratic parametrization of the limb darkening
-def load_limb_coeff(limb_dark_path):
+def load_limb_coeff(limb_dark_path, profile):
+    profile = "square-root" if profile == "squareroot" else profile
+    profile = "custom LD" if (
+            profile == "3-parameter" or
+            profile == "4-parameter"
+    ) else profile
     # Storing the file information into a dataframe
     df = read_csv(limb_dark_path, sep='\s+')
     # Filtering the dataframe to obtain only
     # the one with a quadratic profile
-    row = df[df["profile"] == "quadratic"]
+    row = df[df["profile"] == profile]
     # Mean of the coefficients
     mean_c1 = array(row["c1"], dtype=float).mean()
-    mean_e1 = array(row["e1"], dtype=float).mean()
     mean_c2 = array(row["c2"], dtype=float).mean()
-    mean_e2 = array(row["e2"], dtype=float).mean()
-    return mean_c1, mean_e1, mean_c2, mean_e2
+    mean_c3 = array(row["c3"], dtype=float).mean()
+    mean_c4 = array(row["c4"], dtype=float).mean()
+    return array([mean_c1, mean_c2, mean_c3, mean_c4])
 
 
 def transit_yaml(yaml_params, output_folder_path, input_folder_path):
@@ -46,12 +51,21 @@ def transit_yaml(yaml_params, output_folder_path, input_folder_path):
     # transit time in days
     t14d = t14h / 24
     # limb darkening model
-    params.limb_dark = yaml_params["limb_dark"]
+    csv_profile_name = yaml_params["limb_dark"]
+    # Correction in case of square root name
+    limb_profile = "squareroot" if (
+            csv_profile_name == "square-root"
+    ) else csv_profile_name
+    params.limb_dark = limb_profile
     # Coefficients limb darkening
-    file_limb_dark = str(Path(input_folder_path, yaml_params["file_limb_dark"]))
-    c1, e1, c2, e2 = load_limb_coeff(file_limb_dark)
+    file_limb_dark = str(Path(
+        input_folder_path, yaml_params["file_limb_dark"]
+    ))
+    limb_dark_coeff = load_limb_coeff(
+        file_limb_dark, csv_profile_name
+    )
     # limb darkening coefficients [c1, c2]
-    params.u = [c1, c2]
+    params.u = limb_dark_coeff[~isnan(limb_dark_coeff)]
     limit_transit = t14d / 2 + 0.3 * t14d
     # times at which to calculate light curve (days)
     t = linspace(-limit_transit, limit_transit, 5000)
@@ -63,9 +77,12 @@ def transit_yaml(yaml_params, output_folder_path, input_folder_path):
     plt.plot(t, flux)
     plt.xlabel("Time from central transit")
     plt.ylabel("Relative flux")
-    plt.title(planet_name + " light curve")
+    plt.title(
+        planet_name + " light curve with " + limb_profile +
+        " limb darkening approximation"
+    )
     plt.savefig(str(Path(
         output_folder_path,
-        planet_name + "_assignment1_taskF.png"
+        planet_name + "_" + limb_profile + "_assignment1_taskF.png"
     )))
     plt.show()
